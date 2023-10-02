41f95e1e9030385059b229fefd80dd54d0d3b5c4
rameezrz <76533218+rameezrz@users.noreply.github.com>
2023-10-03 00:44:15 +0530

    Cart service repo created

--- auth_service/main.js
+++ auth_service/main.js
@@ -24,4 +24,4 @@ app.use('/', require('./routes/routes'))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>console.log("server running"))
+app.listen(PORT,()=>console.log("server running hello"))
