f8c15dcdea6d9554f1da98a89fe74bb0872a0c9a
rzeee <rameez007rz@gmail.com>
2023-10-21 20:54:37 +0530

    RabbitMQ implemented for cart and product services

--- cart_service/controllers/addToCart.js
+++ cart_service/controllers/addToCart.js
@@ -1,15 +1,13 @@
 const Cart = require("../models/cartModel");
-const axios = require("axios");
+const amqp = require("amqplib/callback_api");
 
 const addToCart = async (req, res) => {
   try {
     const { productId } = req.body;
-    const userId = req.cookies['userId'];
-    console.log(userId);
-    const { data } = await axios.get(
-      `http://localhost:4002/products/get-product/${productId}`
-    );
-    const { stock } = data.product;
+    const userId = req.cookies["userId"];
+    sendProductId(productId);
+    const product = await getProductDetails();
+    const stock = product?.stock;
     const userCart = await Cart.findOne({ userId });
     if (userCart) {
       let productExist = userCart.products.findIndex((product) => {
@@ -75,5 +73,43 @@ const addToCart = async (req, res) => {
     });
   }
 };
+
+function sendProductId(productId) {
+  try {
+    amqp.connect("amqp://localhost", (err, conn) => {
+      conn.createChannel((err, ch) => {
+        const queue = "cart_to_product";
+
+        ch.assertQueue(queue, { durable: false });
+        ch.sendToQueue(queue, Buffer.from(productId));
+
+        console.log(`Sent ${productId} to ${queue}`);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getProductDetails() {
+  return new Promise((resolve, reject) => {
+    amqp.connect("amqp://localhost", (err, conn) => {
+      conn.createChannel((err, ch) => {
+        const queue = "product_to_cart";
+        ch.assertQueue(queue, { durable: true });
+        console.log("Waiting for product service");
+        ch.consume(
+          queue,
+          (msg) => {
+            console.log(`received ${msg.content.toString()} from ${queue}`);
+            const product = JSON.parse(msg.content.toString());
+            resolve(product);
+          },
+          { noAck: true }
+        );
+      });
+    });
+  });
+}
 
 module.exports = { addToCart };
